8402e896c85c05c1e4a91422736e1719bea07720
VadimZubarev <108616677+VadimZubarev@users.noreply.github.com>
2023-02-24 13:22:02 +0300

    Add files via upload

---
+++ CMakeRobot.cpp
@@ -0,0 +1,11 @@
+﻿// CMakeRobot.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include "CMakeRobot.h"
+
+using namespace std;
+
+void helloWorld()
+{
+	cout << "Hello, World!" << endl;
+}
